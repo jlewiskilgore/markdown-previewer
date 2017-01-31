0a02b22aca23a4a091f597bf3f437efc61249e2b
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-31 18:15:40 -0500

    Update EditWindow starting value text

--- src/components/EditWindow.js
+++ src/components/EditWindow.js
@@ -4,7 +4,7 @@ import marked from 'marked'
 class EditWindow extends Component {
 	constructor(props) {
 		super(props);
-		var startingValue = "# This is a title\n\n### This is a subtitle\n\nYou can make text **bold** or _italicized_ to give more emphasis.\n\n\nLists can be 'Ordered':\n\n1. test 1\n2. test 2\n3. test 3\n...or 'Unordered':\n* another test 1\n* another test 2\n* another test 3\n\n[You can see the source code for this Markdown Previewer at my Github](https://github.com/jlewiskilgore/markdown-previewer)";
+		var startingValue = "# This is a title\n\n### This is a subtitle\n\nYou can make text **bold** or _italicized_ to give more emphasis.\n\n\nLists can be 'Ordered':\n\n1. test 1\n2. test 2\n3. test 3\n\n...or 'Unordered':\n\n* another test 1\n* another test 2\n* another test 3\n\n[You can see the source code for this Markdown Previewer at my Github](https://github.com/jlewiskilgore/markdown-previewer)";
 
 		this.state = { value: startingValue, markedValue: marked(startingValue) };
 
