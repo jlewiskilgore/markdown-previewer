4200e90aef1f0d989db3512d7e96fbdb198d07f4
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-30 19:41:30 -0500

    Render Markdow text below EditWindow as it is being typed

--- src/components/EditWindow.js
+++ src/components/EditWindow.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
+import marked from 'marked'
 
 class EditWindow extends Component {
 	constructor(props) {
 		super(props);
+		var startingValue = "# This is a title\n\n### This is a subtitle\n\nYou can make text **bold** or _italicized_ to give more emphasis.\n\n\nLists can be 'Ordered':\n\n1. test 1\n2. test 2\n3. test 3\n...or 'Unordered':\n* another test 1\n* another test 2\n* another test 3\n\n[You can see the source code for this Markdown Previewer at my Github](https://github.com/jlewiskilgore/markdown-previewer)";
 
-		this.state = { value: '' };
+		this.state = { value: startingValue, markedValue: marked(startingValue) };
 
 		this.handleChange = this.handleChange.bind(this);
 	}
 
 	handleChange(event) {
 		this.setState({value: event.target.value});
+		this.setState({markedValue: marked(event.target.value)});
 	}
 
 	render() {
 		return (
 			<div className="edit-window">
 				<textarea value={this.state.value} onChange={this.handleChange} />
-				<p>{this.state.value}</p>
+				<div id="preview-window" dangerouslySetInnerHTML={{__html: this.state.markedValue }} />
 			</div>
 		);
 	}
--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<EditWindow value="edit text here" />
+				<EditWindow />
 			</div>
 		);
 	}
