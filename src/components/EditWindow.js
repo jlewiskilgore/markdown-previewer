598ab1788503b5706f5d26cc14f0175aefa483dd
John Lewis Kilgore <jlewiskilgore@users.noreply.github.com>
2017-02-01 08:37:05 -0500

    Fix minor formatting

--- src/components/EditWindow.js
+++ src/components/EditWindow.js
@@ -4,7 +4,11 @@ import marked from 'marked';
 class EditWindow extends Component {
 	constructor(props) {
 		super(props);
-		var startingValue = "# This is a title\n\n### This is a subtitle\n\nYou can make text **bold** or _italicized_ to give more emphasis.\n\n\nLists can be 'Ordered':\n\n1. test 1\n2. test 2\n3. test 3\n\n...or 'Unordered':\n\n* another test 1\n* another test 2\n* another test 3\n\n[You can see the source code for this Markdown Previewer at my Github](https://github.com/jlewiskilgore/markdown-previewer)";
+		var startingValue = "# This is a title\n\n### This is a subtitle\n\nYou can make text **bold** or _italicized_ "
+			+ "to give more emphasis.\n\n\nLists can be 'Ordered':\n\n1. test 1\n2. test 2\n3. test 3\n\n"
+			+ "...or 'Unordered':\n\n* another test 1\n* another test 2\n* another test 3\n\n"
+			+ "[You can see the source code for this Markdown Previewer at my Github]" +
+		    	+ "(https://github.com/jlewiskilgore/markdown-previewer)";
 
 		this.state = { value: startingValue, markedValue: marked(startingValue) };
 
@@ -19,8 +23,8 @@ class EditWindow extends Component {
 	render() {
 		return (
 			<div className="markdown-previewer">
-				<textarea id="edit-window" value={this.state.value} onChange={this.handleChange} />
-				<div id="preview-window" dangerouslySetInnerHTML={{__html: this.state.markedValue }} />
+			  <textarea id="edit-window" value={this.state.value} onChange={this.handleChange} />
+			  <div id="preview-window" dangerouslySetInnerHTML={{__html: this.state.markedValue }} />
 			</div>
 		);
 	}
