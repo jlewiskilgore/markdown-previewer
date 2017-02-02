988e1739f51b53bd81318a8a32c68db33256e6cb
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-02-01 21:14:54 -0500

    Fix broken URL

--- src/components/EditWindow.js
+++ src/components/EditWindow.js
@@ -7,8 +7,7 @@ class EditWindow extends Component {
 		var startingValue = "# This is a title\n\n### This is a subtitle\n\nYou can make text **bold** or _italicized_ "
 			+ "to give more emphasis.\n\n\nLists can be 'Ordered':\n\n1. test 1\n2. test 2\n3. test 3\n\n"
 			+ "...or 'Unordered':\n\n* another test 1\n* another test 2\n* another test 3\n\n"
-			+ "[You can see the source code for this Markdown Previewer at my Github]" +
-		    	+ "(https://github.com/jlewiskilgore/markdown-previewer)";
+			+ "[You can see the source code for this Markdown Previewer at my Github](https://github.com/jlewiskilgore/markdown-previewer)\n\n";
 
 		this.state = { value: startingValue, markedValue: marked(startingValue) };
 
